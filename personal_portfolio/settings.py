f2bba9e77d3da79c2ece27e74d454ba08525794b
Alex Zongo <alexanicetzongo@gmail.com>
2020-11-19 23:51:25 +0800

    prepare for deployment on heroku

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    """ 'whitenoise.middleware.WhiteNoiseMiddleware' """
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+""" POJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) """
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
@@ -128,3 +130,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+""" STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') """
